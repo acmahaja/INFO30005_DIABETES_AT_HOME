a6ccc3d5ec74fef24182a19629ae99bb5c0ba897
Your Name <you@example.com>
2022-04-05 16:30:37 +1000

    created health data entry schema

---
+++ models/health_data.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const patient = require("./patient")
+const {Schema} = mongoose;
+
+const healthDataSchema = new mongoose.Schema({
+   to_patient : {
+        type: Schema.Types.ObjectID,
+        ref: 'Patient'
+    },
+    health_type : {
+        type: String,
+        enum: ['blood_glucose', 'weight', 'insulin', 'steps'],
+        required: true
+    },
+    value : {
+        type: Number,
+        required: true
+    },
+    created: {
+        type: Date,
+        required: true
+    },
+    updated: {
+        type: Date,
+        required: true
+    }
+})
+
+
+module.exports = mongoose.model('HealthDataEntry', healthDataSchema);
