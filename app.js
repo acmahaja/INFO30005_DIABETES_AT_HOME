999a99b9ef85628a7289f3e6fea95bbdea23f838
May Myat Noe Htet <maymyatnoeht@student.unimelb.edu.au>
2022-04-06 21:33:30 +1000

    Setting up backend server application
    Rename index.js to app.js. Some security middlewares. Setting up server.

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.engine(
+  'hbs',
+  exphbs.engine({
+    defaultlayout: 'main',
+    extname: '.hbs'
+  })
+);
+app.set('view engine', 'hbs');
+
+// GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Log requests
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Data sanitizaiton against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// ROUTES
+app.get('/clincian', (req, res) => {
+  res.send('Clinician Page');
+});
+
+app.get('/patient', (req, res) => {
+  res.send('Patient Page');
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Diabetes at Home');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
--- index.js
+++ index.js
@@ -3,29 +3,30 @@ const app = express();
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
-app.engine('hbs', exphbs.engine({
-	defaultlayout: 'main',
-	extname: '.hbs'
-}));
+app.engine(
+  "hbs",
+  exphbs.engine({
+    defaultlayout: "main",
+    extname: ".hbs",
+  })
+);
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/clincian', (req,res)=>{
-	res.send("Clinician Page");
-})
+app.get("/clincian", (req, res) => {
+  res.send("Clinician Page");
+});
 
-app.get('/patient', (req,res)=> {
-	res.send("Patient Page");
-})
+app.get("/patient", (req, res) => {
+  res.send("Patient Page");
+});
 
-app.get('/', (req,res)=>{
+app.get("/", (req, res) => {
+  res.send("Welcome to Diabetes at Home");
+});
 
-	res.send("Welcome to Diabetes at Home");
-})
-
-app.listen(3000, ()=>{
-	console.log("Listening on port 3000");
-})
-
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
