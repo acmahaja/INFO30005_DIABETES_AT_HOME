abcd5708c743722f9fb8ad58d51bc778712358b1
Your Name <you@example.com>
2022-04-17 16:18:33 +1000

    Added mongodb support

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const app = express();
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/diabetes-at-home', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	dbName: 'demo'
+})
+
+const db = mongoose.connection.on('error', err => {
+ console.error(err);
+ process.exit(1)
+})
+
+db.once('open', async () => {
+ console.log(`Mongo connected to port ${db.host}:${db.port}`)
+})
+
+
 app.engine('hbs', exphbs.engine({
 	defaultlayout: 'main',
 	extname: '.hbs'
@@ -25,7 +48,7 @@ app.get('/', (req,res)=>{
 	res.send("Welcome to Diabetes at Home");
 })
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT ||3000, ()=>{
 	console.log("Listening on port 3000");
 })
 
