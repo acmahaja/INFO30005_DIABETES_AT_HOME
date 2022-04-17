ca4d0e963cb42810f6de5fc6c685865ccc88973c
Your Name <you@example.com>
2022-04-17 17:07:53 +1000

    Added express-sessions

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const app = express();
+
+const path = require("path")
+
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
+const session = require("express-session")
+
 const mongoose = require("mongoose");
+require('dotenv').config()
 
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
-}
+console.warn("Dev Environment: " + process.env.NODE_ENV);
 
-
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/diabetes-at-home', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	dbName: 'demo'
-})
+mongoose.connect(
+	process.env.NODE_ENV==='production' ? process.env.MONGO_URL : 'mongodb://localhost:27017/diabetes-at-home', 
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		dbName: 'demo'
+	}
+)
 
 const db = mongoose.connection.on('error', err => {
- console.error(err);
- process.exit(1)
+	console.error(err)
+	process.exit(1)
 })
 
 db.once('open', async () => {
- console.log(`Mongo connected to port ${db.host}:${db.port}`)
+ 	console.log(`Mongo connected to port ${db.host}:${db.port}`)
 })
 
 
@@ -32,6 +38,15 @@ app.engine('hbs', exphbs.engine({
 }));
 
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: process.env.SESSION_SECRET
+}));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -44,7 +59,6 @@ app.get('/patient', (req,res)=> {
 })
 
 app.get('/', (req,res)=>{
-
 	res.send("Welcome to Diabetes at Home");
 })
 
