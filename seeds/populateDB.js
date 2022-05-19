09c565b88e9bc7ad5363862b0cf58d95f00f8ea9
Your Name <you@example.com>
2022-05-19 14:40:20 +1000

    removed populate db from index

--- seeds/populateDB.js
+++ seeds/populateDB.js
@@ -24,6 +24,31 @@ const {
 
 const { createHealthData, deleteHealth } = require("./health_data_seed");
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    // process.env.MONGO_URL,
+    'mongodb+srv://admin:healthy@cluster0.fz5ya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    //  "mongodb://localhost:27017/diabetes-at-home",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "diabetes-at-home",
+    }
+  )
+  .then(() => {
+    // clear database on restart
+    // purely for demo purposes
+    console.log(`Mongo connected to port ${db.host}:${db.port}`);
+  });
+
+  
+const db = mongoose.connection.on("error", (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+
 const populateDB = async () => {
   // populate Chris and Pat
   createChris().then((chris) => {
@@ -33,8 +58,8 @@ const populateDB = async () => {
       await createHealthData(pat);
     });
 
-    // give Chris more clinican
-    for (let i = 0; i < 10; i++) {
+    // give Chris more patients
+    for (let i = 0; i < 3; i++) {
       generatePatient(chris).then(async (patient) => {
         await generatePatientSettings(patient);
         await createPatientThresholds(patient);
@@ -43,9 +68,9 @@ const populateDB = async () => {
     }
   });
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 4; i++) {
     generateClinician().then((clincian) => {
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < 4; i++) {
         generatePatient(clincian).then(async (patient) => {
           await generatePatientSettings(patient);
           await createPatientThresholds(patient);
@@ -56,4 +81,12 @@ const populateDB = async () => {
   }
 };
 
-module.exports = { populateDB };
+// module.exports = { populateDB };
+
+const collections = mongoose.connection.collections;
+
+Promise.all(
+  Object.values(collections).map(async (collection) => {
+    await collection.deleteMany({}); // an empty mongodb selector object ({}) must be passed as the filter argument
+  })
+).then(() => populateDB());
